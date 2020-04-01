1e3d47878dfe36e2b71546956c5b12c67f1752fe
Perre <Perre@users.noreply.github.com>
2020-04-02 00:18:41 +0200

    Bom met lont toegevoegd die korter word wanneer het aantal mensen op intensieve dichter bij het maximum komt.

--- public/scriptje.js
+++ public/scriptje.js
@@ -7,7 +7,7 @@ margin = 40;
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
 
-
+var path = null;
 window.addEventListener('load', (event) => {
 
 
@@ -20,13 +20,14 @@ window.addEventListener('load', (event) => {
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-
-
+    path = tekenLont(1);
     updateNumbers();
+    path.setAttribute("d", updateLont(ratio));
+    document.getElementById("bom").appendChild(path);
 });
 
-const maxIntensive = 1.765;
-
+const maxIntensive = 2.220;
+var ratio = 1;
 
 async function getNumbers() {
 
@@ -36,37 +37,44 @@ async function getNumbers() {
     //document.getElementById("infected").textContent = jsonData.geinfecteerd;
 
     // Create data
-    var data = { a: jsonData.belintensive, b: (maxIntensive - jsonData.belintensive) }
-
-    // set the color scale
-    var color = d3.scaleOrdinal()
-        .domain(data)
-        .range(["black", "red"])
-
-    // Compute the position of each group on the pie:
-    var pie = d3.pie()
-        .value(function (d) { return d.value; })
-    var data_ready = pie(d3.entries(data))
-
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-        .selectAll('mySlices')
-        .data(data_ready)
-        .enter()
-        .append('path')
-        .attr('d', d3.arc()
-            .innerRadius(0)
-            .outerRadius(radius)
-        )
-        .attr('fill', function (d) { return (color(d.data.key)) })
-        .attr("stroke", "black")
-        .style("stroke-width", "0px")
-        .style("opacity", 0.7);
+    ratio = jsonData.belintensive/maxIntensive;
+    console.log(ratio);
 }
 
 
 
 function updateNumbers() {
     getNumbers();
-    setTimeout(updateNumbers, 5000);
+    path.setAttribute("d", updateLont(ratio));
+    setTimeout(updateNumbers, 1000);
+}
+
+function tekenLont(ratio) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("fill", "none");
+    path.setAttribute("stroke", "white");
+    path.setAttribute("stroke-width", "50");
+    path.setAttribute("stroke-linecap", "round");
+    path.setAttribute("d", updateLont(ratio));
+    return path;
+}
+
+function updateLont(ratio) {
+    lengte = Math.round(ratio * 100);
+    var dx = 20;
+    var dy = -20;
+    var x = 620;
+    var y = 80;
+    var dh = 0;
+
+    var d = "M620 80";
+    for (let i = 0; i < lengte; i++) {
+        dh += -0.05 + Math.random() * 0.1;
+        dy = Math.sin(dh) * 5;
+        dx = Math.cos(dh) * 5;
+        x += dx;
+        y += dy;
+        d += " L" + x + " " + y;
+    }
+    return d;
 }
