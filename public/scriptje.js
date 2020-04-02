da08c40b4e032fc1ab6e37a6cff7075a73d64185
Perre <Perre@users.noreply.github.com>
2020-04-02 13:05:08 +0200

    Added a spark to the end the fuse

--- public/scriptje.js
+++ public/scriptje.js
@@ -8,25 +8,18 @@ margin = 40;
 var radius = Math.min(width, height) / 2 - margin
 
 var path = null;
+var vlam = null;
 window.addEventListener('load', (event) => {
-
-
-    // append the svg object to the div called 'my_dataviz'
-    svg = d3.select("#content")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("class", "chart")
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
     path = tekenLont(1);
+    vlam = tekenVlam(100, 100);
     updateNumbers();
     path.setAttribute("d", updateLont(ratio));
     document.getElementById("bom").appendChild(path);
+    document.getElementById("bom").appendChild(vlam);
+
 });
 
-const maxIntensive = 2.220;
+const maxIntensive = 2220;
 var ratio = 1;
 
 async function getNumbers() {
@@ -37,16 +30,22 @@ async function getNumbers() {
     //document.getElementById("infected").textContent = jsonData.geinfecteerd;
 
     // Create data
-    ratio = jsonData.belintensive/maxIntensive;
-    console.log(ratio);
+    ratio = jsonData.belintensive / maxIntensive;
+    //console.log(ratio);
 }
 
 
+var t = 0;
 
 function updateNumbers() {
-    getNumbers();
+    t++;
+    if (t > 10) {
+        getNumbers();
+        t = 0;
+    }
     path.setAttribute("d", updateLont(ratio));
-    setTimeout(updateNumbers, 1000);
+    vlam.setAttribute("d", updateVonk(gx, gy));
+    setTimeout(updateNumbers, 100);
 }
 
 function tekenLont(ratio) {
@@ -59,12 +58,42 @@ function tekenLont(ratio) {
     return path;
 }
 
+
+function tekenVlam(cx, cy) {
+    const vpath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    vpath.setAttribute("fill", "orange");
+    vpath.setAttribute("stroke", "none");
+
+    return vpath;
+}
+
+function updateVonk(cx, cy) {
+    const np = 16;
+    const hoek = 2 * Math.PI / np;
+    x = cx + Math.cos(hoek * i) * 50;
+    y = cy + Math.sin(hoek * i) * 50;
+    const r = 50;
+    const dr = 20;
+
+    var d = `M${cx} ${cy}`;
+    for (var i = 0; i < np + 1; i++) {
+
+        x = cx + Math.cos(hoek * i) * (r + i % 2 * (dr+Math.random()*50));
+        y = cy + Math.sin(hoek * i) * (r + i % 2 * (dr+Math.random()*50));
+        d += " L" + x + " " + y;
+    }
+    return d;
+}
+
+var gx = 620;
+var gy = 80;
 function updateLont(ratio) {
     lengte = Math.round(ratio * 100);
     var dx = 20;
     var dy = -20;
-    var x = 620;
-    var y = 80;
+    gx = 620;
+    gy = 80;
+
     var dh = 0;
 
     var d = "M620 80";
@@ -72,9 +101,10 @@ function updateLont(ratio) {
         dh += -0.05 + Math.random() * 0.1;
         dy = Math.sin(dh) * 5;
         dx = Math.cos(dh) * 5;
-        x += dx;
-        y += dy;
-        d += " L" + x + " " + y;
+        gx += dx;
+        gy += dy;
+        d += " L" + gx + " " + gy;
     }
+
     return d;
 }
