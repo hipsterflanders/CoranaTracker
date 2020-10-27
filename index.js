b585261325f82e9a1ec8cfa4a2e8b1dbb2382657
Perre <Perre@users.noreply.github.com>
2020-10-27 20:38:15 +0100

    Databron op server veranderd naar Sciensano

--- index.js
+++ index.js
@@ -14,8 +14,24 @@ app.use(express.json({ limit: '1Gb' }));
 var database = new Datastore('database.db');
 database.loadDatabase();
 
-
+get_sciensanoData();
+setInterval(() => {
+    get_sciensanoData();
+  }, 60000);
 
 app.get('/corona', async (request, response) => {
-    response.json(corona.getData());   
+    //response.json(corona.getData());
+    response.json(total_in_ICU);
 });
+
+var total_in_ICU = {test:"no data from sciensno gotten"};
+
+async function get_sciensanoData() {
+    try{
+        const sciensano_response = await fetch("https://epistat.sciensano.be/Data/COVID19BE_HOSP.json");
+        const sciensano_json = await sciensano_response.json();
+        total_in_ICU = sciensano_json;
+    }catch(err){
+        console.log("Fetching COVID19BE_HOSP.json failed"+err.message);
+    }
+}
